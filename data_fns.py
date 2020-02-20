5f31647a56a7089b2ffe3d68235b3da437ec2694
birajpandey <bpandey@uw.edu>
2020-02-19 22:01:04 -0800

    first commit

---
+++ data_fns.py
@@ -0,0 +1,89 @@
+''' BP: Feb 12, 2020
+Functions for generating data to test random features classifier.
+'''
+
+import numpy as np
+from scipy import signal
+
+def butter_bandpass(lowcut, highcut, fs, order=2):
+    nyq = fs / 2
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = signal.butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=2):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    return signal.lfilter(b, a, data)
+
+def highpass_filter(data, highcut, fs, order=2):
+    nyq = fs/2
+    high = highcut / nyq
+    b, a = signal.butter(order, high, btype='high')
+    return signal.filtfilt(b, a, data)
+
+def lowpass_filter(data, lowcut, fs, order=2):
+    nyq = fs/2
+    low = lowcut / nyq
+    b, a = signal.butter(order, low, btype='low')
+    return signal.filtfilt(b, a, data)
+
+
+def noisy_sin_signal(f_s=2000, dur=10, f_signal=40, sig_dur=0.05, a=1, seed=2):
+    '''
+    Generate a time series of random noise interspersed with periods where a sinusoidal signal is on
+    
+    Parameters
+    ----------
+    
+    f_s = sampling frequency (Hz)
+    dur = duration of the time series (s)
+    f_signal = frequency of the signal (Hz)
+    sig_dur = duration of the signal (s)
+    a: 2x amplitude of the signal used for snr (between 0 and 1)
+    if n_amp = 2 x noise_amplitude: a ** 2 + n_amp ** 2 = 1; snr = a**2/n_amp**2
+    
+    Returns
+    -------
+    time_series: an array of shape (f_s * dur, 1)
+    labels: array with binary values (0, 1) shape (f_s * dur, )
+    '''
+    np.random.seed(seed)
+    
+    if a < 0 or a > 1:
+        raise ValueError('a should be between 0 and 1')
+    n_amp = np.sqrt(1 - a ** 2) 
+    
+    t_points = np.arange(0, dur, 1/f_s)
+    t_series = np.random.normal(0, a /2  , dur * f_s) + np.random.normal(0, n_amp/2, dur * f_s)
+    label = np.zeros(len(t_series))
+    
+    d = int(sig_dur * f_s) # array len of signal
+    p = np.arange(0, len(t_points), d) # array with points d apart
+    idx = np.random.choice(p, int(dur / (2 * sig_dur)), replace=False)
+    for i in idx:
+        t = t_points[i:i + d]
+        t_series[i:i + d] = a / 2 * np.sin(2 * np.pi * f_signal * t) + np.random.normal(0, n_amp / 2, len(t))
+        label[i:i + d] = 1
+    return t_series, label
+
+
+def data_matrix(series, label, N=40):
+    ''' 
+    From a time series, generate a n_sample x n_feature matrix using overlapping windows.
+    
+    Parameters
+    ----------
+    series: 1-D numpy array shape (M, 1)
+    label: 1-D numpy array shape (M, 1)
+    N: n_features of the data matrix
+    
+    Returns
+    -------
+    X: array of shape (M - N, N)
+    y: array of shape (M - N, )
+    '''
+    X = np.array([series[i:i + N] for i in range(len(series) - N)])
+    y = label[N:]
+    return X, y                       
+
---
+++ estimator.py
@@ -0,0 +1,110 @@
+''' BP: Feb 12, 2020
+Random features classifier for time series data'''
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.svm import LinearSVC
+import numpy as np
+import scipy
+
+
+## nonlinearities
+def relu(x, thrsh=0):
+    return np.maximum(x, thrsh)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+class RFClassifier(BaseEstimator, ClassifierMixin):
+    """
+    Random features with SVM classification
+    """
+    
+    def __init__(self, width=500, weights='white noise', nonlinearity=relu, clf=None, weight_fun=None, clf_args={}, 
+                 seed=20):
+        self.width = width
+        self.weights = weights
+        self.nonlinearity = nonlinearity
+        self.clf = clf
+        self.weight_fun = weight_fun
+        self.clf_args = clf_args
+        self.seed = seed
+
+    def fit(self, X, y):
+        # check params
+        if X.shape[0] != y.shape[0]:
+            raise ValueError('dimension mismatch')
+        n = X.shape[1]
+
+        if self.clf is None:
+            self.clf = LinearSVC(random_state=self.seed, tol=1e-4, max_iter=1000)
+        else:
+            self.clf = self.clf(**self.clf_args)
+        
+        if self.weight_fun is not None:
+            self.W_ = self.weight_fun(self.width, n)
+        else:
+            self.W_ = \
+            random_feature_matrix(self.width, n, self.weights, self.seed)
+        H = self.nonlinearity(X @ self.W_)
+        
+        #fit classifier
+        self.clf.fit(H, y)
+        self._fitted = True
+        return self
+
+    def transform(self, X):
+        H = self.nonlinearity(X @ self.W_)
+        return H
+    
+    def predict(self, X):
+        H = self.nonlinearity(X @ self.W_)
+        return self.clf.predict(H)
+    
+    def score(self, X, y):
+        H = self.nonlinearity(X @ self.W_)
+        return self.clf.score(H, y)
+
+def gaussian(X, mu, sigma):
+    return np.exp(-np.abs(mu - X) ** 2/ (2 * sigma **2))
+
+
+def random_feature_matrix(M, N, weights='white noise', rand_seed=20):
+    ''' 
+    Generate a size (M, N) random matrix from the specified distribution
+    
+    Parameters
+    ----------
+    
+    M: number of rows
+    
+    N: number of columns
+    
+    weights: string or function, default 'gaussian.'
+    If 'unimodal', entries are gaussians with means ~ Unif(0, 1), and std. dev ~ Unif(0.1, N).
+    If 'white noise', entries are drawn ~ N(0, 1).
+    Or can be a function handle taking arguments (M, N)
+    '''
+    
+    from numpy.random import randn, uniform, seed
+    
+    seed(rand_seed)
+    if weights == 'unimodal':
+        mu = uniform(0, N, (M, 1))
+        sigma = uniform(0.1, N/4, (M, 1))
+        k = np.arange(0, N)
+        J = np.array([gaussian(k, m, s) for (m, s) in zip(mu, sigma)]) * np.random.randint(1, 20, (M, 1))
+
+    elif weights == 'white noise':
+        J = randn(M, N)
+    
+    elif weights is None:
+        J = np.eye(N, N)
+
+    else:
+        J = weights(M, N)
+    return J.T
+
+
+
+
