4f0f5f3895b7db44951414af31656964e3a95639
birajpandey <bpandey@uw.edu>
2020-08-18 15:14:57 -0400

    updated estimator class and data generating functions

---
+++ new_version/data_fns.py
@@ -0,0 +1,388 @@
+import numpy as np
+import scipy.linalg
+import sklearn.utils as sk_utils
+
+def frequency_detection(n_samples, fs, k, signal_duration, a, 
+    random_state=None): 
+    """
+    Generate frequency detection task. The positive examples are pure
+    sinusoids with additive gaussian noise and the negative examples are 
+    white noise. The examples are generated using the DFT matrix.
+
+    Parameters
+    ----------
+
+    n_samples : int
+        Number of total examples
+
+    fs : int
+        Sampling rate of the signal
+    
+    k : int
+        Column of the DFT matrix. The signal of the frequency is 
+        int(k /  signal_duration)).
+
+    signal_duration: float
+        Length of the signal in seconds.
+
+    a : float, 0 <= a <= 1
+        Determines the SNR of the signal.
+        SNR = a ** 2 / (1 - a ** 2)
+
+    random_state : int
+        Random state of the generated examples
+
+    Returns
+    -------
+    X : (array-like) of shape (n_samples, n_features)
+        Every row corresponds to an example with n_features components.
+        n_features = fs * signal_duration
+
+    y : (array-like) of shape (n_samples,)
+        Target label (-1/1) for every example. 
+    """
+
+    if random_state is not None:
+        np.random.seed(random_state)
+    
+    N = int(fs * signal_duration)
+    noise_amplitude = np.sqrt(1 - a ** 2)
+
+    # dft matrix
+    A = scipy.linalg.dft(N, scale=None)
+
+    # positive examples
+    n_pos = int(n_samples / 2)
+    c = np.zeros((N, n_pos), dtype='complex')
+    rand = np.random.normal(loc=0, scale=1, size=(n_pos, 2)).view(np.complex).flatten()
+    rand /= np.abs(rand)
+    c[k] = rand
+    X_pos = np.sqrt(2) * a * (A @ c).T.real
+
+    # noise for positive egs
+    rand = np.random.normal(loc=0, scale=1, size=(N, n_pos, 2)).view(np.complex).squeeze(axis=2)
+    rand /= np.abs(rand)
+    noise = np.sqrt(2) * noise_amplitude / np.sqrt(N - 1) * (A @ rand).T.real
+    X_pos += noise
+
+    # negative egs
+    n_neg = int(n_samples / 2)
+    c = np.random.normal(loc=0, scale=1, size=(N, n_neg, 2)).view(np.complex).squeeze(axis=2)
+    c /= np.abs(c)
+    X_neg = np.sqrt(2) * (A @ c).T.real / np.sqrt(N)
+
+    # concatenate and shuffle
+    X = np.vstack((X_pos, X_neg))
+    y = np.hstack((np.ones(n_pos), np.ones(n_neg) * -1))
+    X, y = sk_utils.shuffle(X, y)
+
+    return X, y
+
+
+def frequency_XOR(n_samples, fs, k1, k2, signal_duration, a, 
+    random_state=None, shuffle=True):
+    """
+    Generates a frequency XOR task. The positive eg are single
+    frequency sinusoids (2 frequencies) with additive gaussian noise. 
+    The negative eg are mixed sinusoids or white noise.
+
+    Parameters
+    ----------
+
+    n_samples : int
+        Number of total examples
+
+    fs : int
+        Sampling rate of the signal
+    
+    k1 : int
+        Column of the DFT matrix. The frequency of the signal is 
+        int(k1 /  signal_duration)).
+
+    k2 : int
+        Column of the DFT matrix. The frequency of the signal is
+        int(k2 /  signal_duration)). 
+
+    signal_duration: float
+        Length of the signal in seconds.
+
+    a : float, 0 <= a <= 1
+        Determines the SNR of the signal.
+        SNR = a ** 2 / (1 - a ** 2)
+
+    random_state : int
+        Random state of the generated examples
+    
+    shuffle : Bool, default=True
+        Shuffle data if True.
+    
+    Returns
+    -------
+
+    X : (array-like) of shape (n_samples, n_features)
+        Every row corresponds to an example with n_features components.
+        n_features = fs * signal_duration
+
+    y : (array-like) of shape (n_samples,)
+        Target label (-1/1) for every example. 
+    """
+
+    if random_state is not None:
+        np.random.seed(random_state)
+
+    N = int(fs * signal_duration)
+    noise_amplitude = np.sqrt(1 - a ** 2)
+
+    #dft matrix
+    A = scipy.linalg.dft(N, scale=None)
+
+    # positive examples
+    n_pos = int(n_samples/2)
+    c = np.zeros((N, n_pos), dtype='complex')
+    rand = np.random.normal(loc=0, scale=1, size=(int(n_pos/ 2), 2)).view(np.complex).flatten()
+    rand /= np.abs(rand)
+    c[k1, :int(n_pos/2)] = rand
+
+    rand = np.random.normal(loc=0, scale=1, size=(int(n_pos/ 2), 2)).view(np.complex).flatten()
+    rand /= np.abs(rand)
+    c[k2, int(n_pos/2):] = rand
+    X_pos = np.sqrt(2) * a * (A @ c).T.real
+
+    # noise for positive egs
+    rand = np.random.normal(loc=0, scale=1, size=(N, n_pos, 2)).view(np.complex).squeeze(axis=2)
+    rand /= np.abs(rand)
+    noise = np.sqrt(2) * noise_amplitude / np.sqrt(N - 1) * (A @ rand).T.real
+    X_pos += noise
+
+    # negative egs
+    n_neg = int(n_samples/2)
+
+    # mixed egs
+    c = np.zeros((N, int(n_neg/2)), dtype='complex')
+    rand = np.random.normal(loc=0, scale=1, size=(1, int(n_neg/2), 2)).view(np.complex).squeeze(axis=2)
+    rand /= np.abs(rand)
+    c[[k1, k2]] = rand
+    X_mixed = a * (A @ c).T.real
+
+    # noise for mixed egs
+    rand = np.random.normal(loc=0, scale=1, size=(N, int(n_neg/2), 2)).view(np.complex).squeeze(axis=2)
+    rand /= np.abs(rand)
+    noise = np.sqrt(2) * noise_amplitude / np.sqrt(N - 2) * (A @ rand).T.real
+    X_mixed += noise
+
+    # noise as negative egs
+    c = np.random.normal(loc=0, scale=1, size=(N, int(n_neg/2), 2)).view(np.complex).squeeze(axis=2)
+    c /= np.abs(c)
+    X_noise = np.sqrt(2) * (A @ c).T.real / np.sqrt(N)
+    
+    X_neg = np.row_stack((X_mixed, X_noise))
+    
+    # concatenate and shuffle
+    X = np.vstack((X_pos, X_neg))
+    y = np.hstack((np.ones(n_pos), np.ones(n_neg) * -1))
+    if shuffle is True:
+        X, y = sk_utils.shuffle(X, y)
+    return X, y
+
+
+def load_mnist(data_path='./data/MNIST'):
+    """
+    Loads the MNIST handwritten digits data. There are 60k training
+    images and 10k testing images. Each image is (28, 28). The digits 
+    are from 0-10.
+
+    Parameters
+    ----------
+    data_path : string, default='./data/MNIST/'
+        Path to the MNIST folder.
+    
+    Returns
+    -------
+    train : (array-like) of shape (60000, 784)
+        Training data
+
+    train_labels : (array-like) of shape (60000,)
+        Training labels
+
+    test : (array-like) of shape (10000, 784)
+        Testing data
+
+    test_labels : (array-like) of shape (10000,)
+        Test data labels
+    """
+    from mnist import MNIST
+    mndata = MNIST(data_path)
+    train, train_labels = map(np.array, mndata.load_training())
+    test, test_labels = map(np.array, mndata.load_testing())
+    train = train/255
+    test = test/255
+    return train, train_labels, test, test_labels
+
+def unpickle(file):
+    """
+    Open a pickle file to a dict. This is specifically for cifar_10 data. 
+
+    Parameters
+    ----------
+
+    file : string
+        Path of the pickle file.
+    
+    Returns
+    -------
+
+    dict : dict
+        Pickle file as a dictionary
+    """
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+
+def load_cifar_10(data_path='./data/cifar_10/', grayscale=True):
+    """
+    Loads the CIFAR-10 dataset. There are 50k training images and 10k
+    test images. There are 10 categories. Each image is (32, 32, 3). 
+
+    Parameters
+    ----------
+
+    file : string
+        Path to the cifar10 folder
+
+    grayscale : bool, default=True
+        Converts the color image into grayscale using Luma coding
+
+    Returns
+    -------
+    train : (array-like) of shape (50000, n_features)
+        Training data
+
+    train_labels : (array-like) of shape (50000, n_features)
+        Training labels
+
+    test : (array-like) of shape (10000, n_features)
+        Testing data
+
+    test_labels : (array-like) of shape (10000,)
+        Test data labels
+    """
+    train = np.empty((0, 3072))
+    train_labels = []
+    for i in range(5):
+        batch = unpickle(data_path + 'data_batch_%s' % str(i + 1))
+        train = np.row_stack((train, batch[b'data']))
+        train_labels = np.concatenate((train_labels, batch[b'labels']))
+
+    test_batch = unpickle(data_path + '/test_batch')
+    test = test_batch[b'data']
+    test_labels = np.array(test_batch[b'labels'])
+
+    if grayscale is True:
+        train = 0.2989 * train[:, :1024] + 0.5870 * train[:, 1024:2048] + 0.1140 * train[:, -1024:]
+        test = 0.2989 * test[:, :1024] + 0.5870 * test[:, 1024:2048] + 0.1140 * test[:, -1024:]
+    return train, train_labels, test, test_labels
+
+def load_kmnist(data_path='./data/kmnist/'):
+    """ 
+    Loads the Kuzushiji-MNIST dataset. The dataset is downloaded in the
+    numpy version.
+
+    Parameters
+    ----------
+    
+    data_path : string
+        Path to the kmnist data
+
+    Returns
+    -------
+    train : (array-like) of shape (50000, n_features)
+        Training data
+
+    train_labels : (array-like) of shape (50000, n_features)
+        Training labels
+
+    test : (array-like) of shape (10000, n_features)
+        Testing data
+
+    test_labels : (array-like) of shape (10000,)
+        Test data labels
+    """
+    train = np.load(data_path + 'kmnist-train-imgs.npz')['arr_0']
+    train_labels = np.load(data_path + 'kmnist-train-labels.npz')['arr_0']
+    test = np.load(data_path + 'kmnist-test-imgs.npz')['arr_0']
+    test_labels = np.load(data_path + 'kmnist-test-labels.npz')['arr_0']
+
+    train = train.reshape(-1, 784)
+    test = test.reshape(-1, 784)
+    train = train /255
+    test = test /255
+
+    return train, train_labels, test, test_labels
+
+def download_omniglot(data_path='./data'):
+
+    """
+    Downloads the omniglot dataset. The dataset contains images of 1623 
+    different characters with 20 examples of each character. Each image
+    is of size (105, 105). We will load only the training set with 964 characters. It's the "background_images" in the original dataset.
+    The images are saved in the datapath in a dict with labels 'data'
+    and 'labels.'
+
+    Parameters
+    ----------
+
+    data_path = string  
+        Path to download data into
+
+    Returns
+    -------
+    None
+
+    """
+    import torchvision as tv
+    from torch.utils.data import DataLoader
+    import pickle
+
+    dataset = tv.datasets.Omniglot(root=data_path, download=True, 
+    transform=tv.transforms.ToTensor())
+    dataloader = DataLoader(dataset, batch_size=len(dataset))
+    data = next(iter(dataloader))[0].numpy().squeeze()
+    labels = next(iter(dataloader))[1].numpy()
+    dict = {'data': data, 'labels': labels}
+
+    with open(data_path + '/omniglot-py/omniglot.pickle', 'wb') as handle:
+        pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def load_omniglot(data_path='./data/omniglot-py/'):
+    """
+    Loads the omniglot dataset. The dataset contains images of 1623 
+    different characters with 20 examples of each character. Each image
+    is of size (105, 105). We will load only the training set with 964 characters. Download the data first with `download_omniglot` before
+    using this function. 
+
+    Parameters
+    ----------
+
+    data_path = string
+        Path to the `omniglot-py` data folder
+
+    Returns
+    -------
+    data : (array-like) of shape (964 * 20, 11025)
+        Training data
+
+    label : (array-like) of shape (964 * 20,)
+        Target labels
+    """
+    import pickle
+
+    with open(data_path + 'omniglot.pickle', 'rb') as handle:
+        data_dict = pickle.load(handle)
+    return data_dict['data'], data_dict['labels']
+
+
+
+
---
+++ new_version/estimator.py
@@ -0,0 +1,426 @@
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_is_fitted
+import scipy.linalg
+from scipy.spatial.distance import pdist, squareform
+import numpy as np
+import numpy.linalg as la
+
+class RFClassifier(BaseEstimator, ClassifierMixin):
+    """
+    Random feature classifier.
+
+    This class projects inputs onto randomly generated weights and 
+    classifies them using a linear classifier.
+
+    Parameters
+    ----------
+
+    width : int
+        Number of random weights the input is 
+        projected onto.
+
+    nonlinearity: callable
+        Specifies the non-linear transformation that is 
+        applied to the randomly transformed inputs. The call signature
+        is ``fun(x)``.
+    
+    weight_fun: callable
+        Specifies the function to generate the random weights. The call signature is 
+        ``fun(width, n_features, **kwargs, random_state). ``
+        Here, `width` is a positive int and specifies the 
+        number of random weights. Here, `n_features` is a positive 
+        int and specifies the size of each random weight. `fun`
+        must return array_like with shape (width, n_features) i.e.
+        each row corresponds to a random weight.
+
+    kwargs: dict, optional
+        Additional arguments to be passed to the weight
+        function. If for example if the weight function has the 
+        signature ```fun(width, n_features, a, b, c, random_state, )```, 
+        then `kwargs` must be a dict with three parameters and their
+        keywords. 
+    
+    bias: ndarray of shape (width,) or (1,)
+        The bias term for the randomly transformed input. If (1,), same
+        bias is used for all the random weights.
+
+    clf : sklearn linear classifier object, eg: logistic regression, 
+        linear SVM. Specifies the linear classifier used for 
+        classification.
+
+    random_state: int, default=None
+        Used to set the seed when generating random weights.
+    
+    Attributes
+    -------
+
+    W_ : ndarray of shape (width, n_features)
+        Random weights that are generated.
+
+    b_ : ndarray of shape (width, ) or (1,)
+        The bias term for the randomly transformed input.
+
+    H_ : {array-like} of shape (n_samples, width)
+        Transformed train input, where n_samples is the number of samples
+        and width is the number of features.
+
+    coef_ : ndarray of shape (1, n_features) or (n_classes, n_features)
+        Coefficient of the features in the decision function of the 
+        classifier.
+    
+    intercept_ : ndarray of shape (1,) or (n_classes,)
+        Intercept (a.k.a. bias) added to the decision function of the
+        classifier.
+
+    Examples
+    --------
+
+    from sklearn.datasets import load_digits
+    from sklearn.linear_model import LogisticRegression
+    from estimator import RFClassifier, V1_inspired_weights
+
+    X, y = load_digits(return_X_y=True)
+    logit = LogisticRegression(solver='saga')
+    relu = lambda x: np.maximum(0, x)
+    kwargs = {'t': 5, 'l': 3}
+    clf = RFClassifier(width=20, weight_fun=V1_inspired_weights, 
+    kwargs=kwargs, bias=2, nonlinearity=relu, clf=logit, random_state=22)
+    clf.fit(X, y)
+    clf.score(X, y)
+    
+    """
+
+    def __init__(self, width, weight_fun, bias, nonlinearity, 
+    clf, kwargs=None, random_state=None):
+        self.width = width
+        self.nonlinearity = nonlinearity
+        self.weight_fun = weight_fun
+        self.bias = bias
+        self.clf = clf
+        self.kwargs = kwargs
+        self.random_state = random_state
+    
+    def _fit_transform(self, X):
+        """
+        Project the train input onto the random weights.
+
+        Parameters
+        ----------
+        X : {array-like} of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of sampless
+            and n_features is the number of features.
+
+        Returns
+        -------
+        self 
+        
+        """
+        n_features = X.shape[1]
+        if self.kwargs is not None:
+            self.W_ = self.weight_fun(self.width, n_features, 
+                                        **self.kwargs, random_state=self.random_state)
+        else:
+            self.W_ = self.weight_fun(self.width, n_features, 
+                            random_state=self.random_state)
+
+        self.b_ = self.bias
+        self.H_ = self.nonlinearity(np.dot(X, self.W_.T) + self.b_)
+
+    def fit(self, X, y):
+        """
+        Fit the model according to the given training data. 
+
+        Parameters
+        ----------
+        X : {array-like} of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of samples
+            and n_features is the number of features.
+
+        y : array-like of shape (n_samples,)
+            Target vector relative to X.
+
+        Returns
+        -------
+        self
+            Fitted estimator
+        """
+        self._fit_transform(X)
+        self.clf.fit(self.H_, y)
+        self.coef_ = self.clf.coef_
+        self.intercept_ = self.clf.intercept_
+        return self
+
+    def transform(self, X):
+        """
+        Project test input onto the random weights.
+
+        Parameters
+        ----------
+
+        X : {array-like} of shape (n_samples, n_features)
+            Training vector, where n_samples is the number of sampless
+            and n_features is the number of features.
+
+        Returns
+        -------
+
+        H : {array-like} of shape (n_samples, width)
+            Transformed input, where n_samples is the number of samples
+            and n_features is the number of features.
+        """
+        check_is_fitted(self, ["W_", "b_"])
+        H = self.nonlinearity(np.dot(X, self.W_.T) + self.b_)
+        return H
+
+    def score(self, X, y):
+        """
+        Returns the score on the given test data and labels.
+
+        Parameters
+        ---------
+        X : array-like of shape (n_samples, n_features)
+            Test samples.
+        
+        y : array-like of shape (n_samples,)
+            True labels for X.
+
+        Returns
+        ------
+        score : float
+        """
+        H = self.transform(X)
+        check_is_fitted(self, ["coef_", "intercept_"])
+        score = self.clf.score(H, y)
+        return score
+        
+
+def classical_weights(M, N, random_state=None):
+    """
+    Generates classical random weights. W ~ N(0, 1).
+
+    Parameters
+    ----------
+
+    M : int
+        Number of random weights
+
+    N : int
+        Number of features
+    
+    random_state : int, default=None
+        Used to set the seed when generating random weights.
+
+    Returns
+    -------
+
+    W : array-like of shape (M, N)
+        Random weights.
+    """
+    np.random.seed(random_state)
+    dft = scipy.linalg.dft(N, scale=None)
+    rand = np.random.normal(0, 1, size=(M, N, 2)).view(np.complex).squeeze(axis=2)
+    W = np.dot(rand, dft).real
+    W /= np.std(W, axis=1).reshape(-1, 1)
+    return W
+
+def haltere_inspired_weights(M, N, lowcut, highcut, random_state=None):
+    """
+    Generates random weights with tuning similar to mechanosensory 
+    neurons in insect halteres. Weights are band-limited gaussian 
+    process with fourier eigenbasis.
+
+    Parameters
+    ----------
+
+    M : int
+        Number of random weights
+
+    N : int
+        Number of features
+
+    lowcut: int, column of the (N x N) DFT matrix
+        Low end of the frequency band. 
+
+    highcut: int, column of the (N x N) DFT matrix
+        High end of the frequency band.
+    
+    random_state : int, default=None
+        Used to set the seed when generating random weights.
+    
+    Returns
+    -------
+
+    W : array-like of shape (M, N)
+        Random weights.
+    """
+    np.random.seed(random_state)
+    dft = scipy.linalg.dft(N, scale=None)
+    rand = np.zeros((M, N), dtype=complex)
+    rand[:, lowcut:highcut] = np.random.normal(0, 1, 
+                size=(M, highcut-lowcut, 2)).view(np.complex).squeeze()
+    W = np.dot(rand, dft).real
+    W /= np.std(W, axis=1).reshape(-1, 1)
+    return W
+
+
+def V1_inspired_kernel_matrix(N, t, l, m):
+    """
+    Generates the (N x N) kernel matrix for Gaussian Process with non-stationary 
+    covariance. This kernel matrix will be used to generate random 
+    features inspired from the receptive-fields of V1 neurons.
+
+    K(x, y) = exp(|x - y|^2/2t) * exp(|x - m|^2/2l) * exp(|y - m|^2/2l)
+
+    Parameters
+    ----------
+
+    N : int
+        Number of features 
+
+    t : float
+        Determines the spatial frequency of the random weights
+
+    l : float
+        Determines the width of the random weights 
+    
+    m : tuple (2, 1)
+        Determines the center of the random weights.
+
+    Returns
+    -------
+
+    K : array-like of shape (N, N)
+        Kernel matrix
+    """
+    x = np.arange(np.sqrt(N))
+    yy, xx = np.meshgrid(x, x)
+    grid = np.column_stack((xx.flatten(), yy.flatten()))
+    
+    a = squareform(pdist(grid, 'sqeuclidean'))
+    b = la.norm(grid - m, axis=1) ** 2
+    c = b.reshape(-1, 1)
+    K = 10 * np.exp(-a / (2 * l ** 2)) * np.exp(-b / (2 * t ** 2)) * np.exp(-c / (2 * t ** 2))
+    K += 1e-5 * np.eye(N)
+    return K
+
+
+def V1_inspired_weights_for_center(N, t, l, m, random_state=None):
+    """
+    Generates a random weight for one given center by sampling a 
+    non-stationary Gaussian Process.
+
+    Parameters
+    ----------
+
+    N : int
+        Number of features 
+
+    t : float
+        Determines the spatial frequency of the random weights
+
+    l : float
+        Determines the width of the random weights 
+    
+    m : tuple (2, 1)
+        Determines the center of the random weights.
+
+     random_state : int, default=None
+        Used to set the seed when generating random weights.
+
+    Returns
+    -------
+
+    W : (array-like) of shape (N,)
+        A random weight
+    """
+    np.random.seed(random_state)
+    K = V1_inspired_kernel_matrix(N, t, l, m)
+    L = la.cholesky(K)
+    W = np.dot(L, np.random.randn(N))
+    return W
+
+def V1_inspired_weights(M, N, t, l, random_state=None):
+    """
+    Generate random weights inspired by the tuning properties of the 
+    neurons in Primary Visual Cortex (V1).
+
+    Parameters
+    ----------
+
+    M : int
+        Number of random weights
+
+    N : int
+        Number of features
+    
+    t : float
+        Determines the spatial frequency of the random weights
+
+    l : float
+        Determines the width of the random weights 
+    
+    m : tuple (2, 1)
+        Determines the center of the random weights.
+
+    random_state : int, default=None
+        Used to set the seed when generating random weights.
+
+    Returns
+    -------
+
+    W : array-like of shape (M, N)
+        Random weights.
+
+    """
+    np.random.seed(random_state)
+    centers = np.random.randint(int(np.sqrt(N)), size=(M, 2))
+
+    W = np.empty(shape=(0, N))
+    for m in centers:
+        w = V1_inspired_weights_for_center(N, t, l, m, 
+                                            random_state=random_state)
+        W = np.row_stack((W, w.T))
+    return W
+    
+
+def relu(x, thrsh=0):
+    """
+    Rectified Linear Unit
+
+    Parameters
+    ----------
+
+    x : {array-like} or int
+        Input data
+    
+    thrsh: int
+        threshold for firing
+
+    Returns
+    -------
+
+    y : {array-like} or int
+        Output
+    """
+    return np.maximum(x, thrsh)
+
+def poly(x, power=2):
+    """
+    Polynomial function. Raises input to specified power.
+
+    Parameters
+    ----------
+
+    x : {array-like} or int
+        Input
+    
+    power: int
+        Degree of the polynomial
+
+    Returns
+    -------
+
+    y : {array-like} or int
+        Output
+    """
+    return np.power(x, power)
